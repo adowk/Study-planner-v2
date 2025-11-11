71a8ec41ce7b21db2e1c445777b1862b6f55e9b1
henrikengel-bit <henrik.engelsvoll@gmail.com>
2025-11-11 09:56:34 +0100

    gjort oppgavene
    Kan justere i seneere anleding men har lagt grunnlaget.

--- functions.py
+++ functions.py
@@ -10,7 +10,6 @@ class Emne:
         semnavn = "Høst" if self.semester == "H" else "Vår"
         return f"{self.kode}: {self.navn} ({self.studiepoeng} studiepoeng, {semnavn})"
 
-
 emner = []
 
 
@@ -18,6 +17,33 @@ def lag_nytt_emne(kode, semester, studiepoeng, navn):
     nytt_emne = Emne(kode, semester, studiepoeng, navn)
     emner.append(nytt_emne)
 
+
+# Oppgave 2
+studieplan = [] 
+
+def legg_til_i_studieplan(emne):
+    e = lag_nytt_emne(emne.kode, emne.semester, emne.studiepoeng, emne.navn)
+
+    if e is None: 
+        print("Emnet ble ikke funnet og ikke lagt til å studieplanen.")
+        return False
+    if ann(x.kode == emne.kode for x in studieplan):
+        print("Emnet er allerede i studieplanen.")
+        return False
+    studieplan.append(e)
+    print(f"Emnet {emne.kode} ble lagt til i studieplanen.")
+    return True
+
+
+# Oppgave 3
+def fjern_fra_studieplan(emne):
+    if emne in studieplan:
+        studieplan.remove(emne)
+        print(f"Emnet {emne.kode} ble fjernet fra studieplanen.")
+        return True
+    else:
+        print(f"Emnet {emne.kode} finnes ikke i studieplanen.")
+        return False
 
 # Oppgave 4 
 def skriv_ut_emner():
@@ -27,6 +53,50 @@ def skriv_ut_emner():
         for e in emner:
             print(e)
 
+
+# Oppgave 5
+def ny_studieplan():
+    global studieplan
+    studieplan = []
+    print("En ny studieplan er opprettet.")
+
+# Oppgave 6
+def skriv_ut_studieplan():
+    if not studieplan:
+        print("Studieplanen er tom.")
+        return False
+
+hostemner = [e for e in studieplan if e.semester == "H" or e.semester == "Høst"]
+varemner = [e for e in studieplan if e.semester == "V" or e.semester == "Vår"]
+
+if hostemner:
+    for e in hostemner:
+        print(" -", e)
+    else:
+        print("ingen emner i dette semesteret.")
+
+
+if varemner:
+    for e in varemner:
+        print(" -", e)
+    else: 
+        print("ingen emner i dette semesteret.")
+
+# Oppgave 7
+def sjekk_studieplan():
+    if not self.kode:
+        print("Studieplanen er tom.")
+        return False
+    if self.semester not in ["H", "V"]:
+        print("Ugyldig semesterkode funnet i studieplanen.")
+        return False
+    if self.studiepoeng <= 0:
+        print("Ugyldig studiepoeng funnet i studieplanen.")
+        return False
+    if not self.navn.strip():
+        print("Ugyldig emnenavn funnet i studieplanen.")
+        return False
+    return True
 
 # Oppgave 9
 def lagre_til_fil(filnavn="emner.txt"):
